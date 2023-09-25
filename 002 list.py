c89f21d5b8aaacf82f9eb407d1b1e67ad48a0449
HeathGriffith <heathgriff@gmail.com>
2023-09-25 14:32:57 -0500

    empty, indices, slicing

---
+++ 002 list.py
@@ -0,0 +1,12 @@
+empty_list = []
+top_cities = ['NYC', 'LA', 'Chicago', 'Houston', 'Phoenix']
+print(top_cities)
+# indexing
+print(top_cities[0])
+print(top_cities[-1] + ": this is the last element")
+#slicing [first element to include : first element to not include]
+print(top_cities[1:4])
+'''
+take all indices starting at two -- top_cities[2:]
+take all elements from start to index three (exclusive) -- top_cities[:3] 
+'''
