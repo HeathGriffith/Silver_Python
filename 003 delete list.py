75b72258cf1d1531f6ebe268e772c66569e7503d
HeathGriffith <heathgriff@gmail.com>
2023-09-25 14:39:53 -0500

    deleting with lists

---
+++ 003 delete list.py
@@ -0,0 +1,8 @@
+top_cities = ['NYC', 'LA', 'Singapore', 'Chicago', 'Houston', 'Phoenix']
+del top_cities[2]
+print(top_cities[2])
+del top_cities[3:]
+print(top_cities)
+'''
+del top_cities[:] -- deletes all elements
+del top_cities -- deletes the list'''
