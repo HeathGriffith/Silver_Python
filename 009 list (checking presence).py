a2dcf3588ed50ba0b0a3c88ac3fbb2258cb7d46f
HeathGriffith <heathgriff@gmail.com>
2023-09-27 05:55:28 -0500

    in and not in

---
+++ 009 list (checking presence).py
@@ -0,0 +1,13 @@
+# *in* operator checks every element in string 
+for char in 'happy message':
+    print(char)
+
+# *in* operator checks every element in list
+invited_guests = ['Kate', 'Adam', 'Kerry', 'Joe', 'Anne', 'Marie']
+name = input('What is your name? ')
+if name in invited_guests:
+    print('Welcome!')
+else:
+    print('You are not invited!')
+
+    #also works: 8. *if name not in invited_guests:
