0c09c7cc0a553ba6170db9c2ce1409849db715bb
HeathGriffith <heathgriff@gmail.com>
2023-09-27 07:24:50 -0500

    short demo

---
+++ 013 add-and-multiply lists.py
@@ -0,0 +1,12 @@
+# add list
+list_us = ['NY', 'ATX']
+list_uk = ['London', 'Bristol']
+list_all = list_us + list_uk
+print(list_all)
+
+# multiply list
+repeat_us = list_us * 2
+print(repeat_us)
+
+list_numbers = [10, 23] * 4
+print(list_numbers)
