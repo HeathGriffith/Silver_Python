a10f5841ff0f1d9716f9211309f2d487b731a244
HeathGriffith <heathgriff@gmail.com>
2023-09-29 07:35:11 -0500

    info

---
+++ 019 Dictionaries intro.py
@@ -0,0 +1,11 @@
+# dictionaries are collections to store key-value pairs
+
+# show value by providing key within []; can't provide value to find key
+emails = {
+    'Heath Griffith': 'heath@gmail.com',
+    'Macrina Garza': 'garzamac@gmail.com',
+    'Macky Griffith': 'goomer@gmail.com'
+}
+print(emails['Heath Griffith'])
+
+# keys can be of any immutable data type (e.g., integers, floats, tuples -- not lists)
