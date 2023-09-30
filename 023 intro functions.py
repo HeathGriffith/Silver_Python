d75706f8d9017865d50a39d77894d9a5013f3555
HeathGriffith <heathgriff@gmail.com>
2023-09-30 05:31:40 -0500

    short intro

---
+++ 023 intro functions.py
@@ -0,0 +1,12 @@
+# 'greet' is the function name, same rules for naming as variables; round brackets empty in this case
+def greet():
+    # single indented instruction that will be executed every time function is invoked (call)
+    print('Hello my dear!')
+
+# call it
+greet()
+# call it as many times as like
+greet()
+greet()
+
+# keep all function defs at top of file, as good practice
