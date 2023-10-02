0df71e141d8ce092dbefb85eedf1d838381f64f7
HeathGriffith <heathgriff@gmail.com>
2023-10-02 05:46:26 -0500

    alternative for readability

--- 022 HR list commands.py
+++ 022 HR list commands.py
@@ -73,3 +73,26 @@ while counter < N:
     elif command[0] == commands[6]:
         output.reverse()
     counter += 1 
+
+# strings instead of commands list to know which command is checked in each condition:
+output = []
+N = int(input())
+counter = 0
+
+while counter < N:
+    command = input().split()
+    if command[0] == 'insert':
+        output.insert(int(command[1]), int(command[2]))
+    elif command[0] == 'print':
+        print(output)
+    elif command[0] == 'remove':
+        output.remove(int(command[1]))
+    elif command[0] == 'append':
+        output.append(int(command[1]))
+    elif command[0] == 'sort':
+        output.sort()
+    elif command[0] == 'pop':
+        output.pop()
+    elif command[0] == 'reverse':
+        output.reverse()
+    counter += 1
