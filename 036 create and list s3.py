7d8308eb1ca6ecc7aa666c507b6b121f76e37fef
HeathGriffith <heathgriff@gmail.com>
2023-10-05 05:23:54 -0500

    added course notes

--- 036 create and list s3.py
+++ 036 create and list s3.py
@@ -1,10 +1,24 @@
 import boto3
 
-# Initialize a session using Amazon S3
-session = boto3.Session(profile_name='iamadmin-heath3')
-s3 = session.client('s3')
+# main three boto3 interfaces (ways boto3 communicates with AWS):
 
-# Create a new bucket
+
+# (1) Session object to connect directly to AWS. Pass in keys as starting point. Initialize a session using Amazon S3
+session = boto3.Session(profile_name='iamadmin-heath3')
+
+# (2) client interface: talk about specific service, 
+# can call directly from session
+s3 = session.client('s3')
+#can also call directly from boto3 [most of time]: s3 = boto3.client('s3)
+
+# (3) resource interface: s3 = boto3.resource('s3') 
+# can also be directly called from session: s3 = session.resource('s3') 
+#   [things come back like an api, similar to Python classes]
+#  can be directly called from session as well
+
+# client gives more API type responses; resource, more Python way: we'll be using client
+
+# The `create_bucket` method is called on the `s3` client object, 
 s3.create_bucket(Bucket='testing-a-bucket-321')
 
 # List all buckets
