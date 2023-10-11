2bf7206dfdeb9642aa4b0e59559f66a6434c5400
Heath Griffith <heathgriff@gmail.com>
2023-10-11 12:37:24 -0500

    rename for order

--- Boto3/041 make object public.py
+++
@@ -1,9 +0,0 @@
-# find put acl in boto3 doc
-
-import boto3
-s3 = boto3.client('s3')
-
-response = s3.put_object_acl(ACL='public-read', Bucket = 'hgriffith-boto3-10052023', Key = 'test_text.txt')
-print(response)
-
-# must turn off *block public access* on bucket
