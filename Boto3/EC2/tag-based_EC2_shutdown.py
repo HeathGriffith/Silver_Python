44f2d66e476e9759d731a764a845ae7cb217adaf
Heath Griffith <heathgriff@gmail.com>
2023-10-22 06:11:34 -0500

    change function name 'stop_instances' to 'stop_select_instances' to avoid confusion with the client object 'stop_instances' method

--- Boto3/EC2/tag-based_EC2_shutdown.py
+++ Boto3/EC2/tag-based_EC2_shutdown.py
@@ -1,6 +1,5 @@
 import boto3
 
-
 # Handle exceptions
 def handle_exception(e, context):
     print(f"An error occurred in {context}: {e}")
@@ -27,12 +26,12 @@ def get_dev_instance_ids(ec2_client):
                 dev_instance_ids.append(instance['InstanceId'])
         return dev_instance_ids
     except Exception as e:
-        handle_exception(e, "fetching Dev instances")
+        handle_exception(e, "getting 'Dev' instances")
         return []
 
 
 # Stop instances
-def stop_instances(ec2_client, instance_ids):
+def stop_select_instances(ec2_client, instance_ids):
     try:
         ec2_client.stop_instances(InstanceIds=instance_ids)
         print(f"Successfully stopped instances: {instance_ids}")
@@ -44,6 +43,6 @@ if __name__ == "__main__":
     ec2_client = boto3.client('ec2', region_name='us-east-1')
     dev_instance_ids = get_dev_instance_ids(ec2_client)
     if dev_instance_ids:
-        stop_instances(ec2_client, dev_instance_ids)
+        stop_select_instances(ec2_client, dev_instance_ids)
     else:
         print("No 'Dev' instances to stop.")
