ab178c2d6bd8943230039ef4fa09b6a86744e0db
Heath Griffith <heathgriff@gmail.com>
2023-11-03 06:28:12 -0500

    added inline comments to SQS creation

--- Boto3/Lambda/create_sqs.py
+++ Boto3/Lambda/create_sqs.py
@@ -1,11 +1,11 @@
 import boto3
 
-def create_sqs_queue(queue_name):
-   sqs = boto3.client('sqs')
-   response = sqs.create_queue(QueueName=queue_name)
-   queue_url = response['QueueUrl']
-   print(f'Queue URL: {queue_url}')
+def create_sqs_queue(queue_name):  # Define a function to create an SQS queue with a specified name
+   sqs = boto3.client('sqs')  # Initialize an SQS client using boto3
+   response = sqs.create_queue(QueueName=queue_name)  # Create the queue and store the response
+   queue_url = response['QueueUrl']  # Extract the queue URL from the response
+   print(f'Queue URL: {queue_url}')  # Print the queue URL
 
-if __name__ == "__main__":
-   queue_name = "LUIT-queue"
-   create_sqs_queue(queue_name)
+if __name__ == "__main__":  # Check if this script is being run directly
+   queue_name = "LUIT-queue"  # Set the name of the queue
+   create_sqs_queue(queue_name)  # Call the function to create the queue
